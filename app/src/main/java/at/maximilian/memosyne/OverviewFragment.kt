714a5bd3f73e3fbbc6dd4054e1128c899a0bd520
magical-obama <maschwaerzler@a1.net>
2022-09-29 21:45:02 +0200

    Remove boilerplate second fragment

--- app/src/main/java/at/maximilian/memosyne/OverviewFragment.kt
+++ app/src/main/java/at/maximilian/memosyne/OverviewFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import at.maximilian.memosyne.databinding.FragmentOverviewBinding
 
@@ -33,7 +34,7 @@ class OverviewFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_OverviewFragment_to_SecondFragment)
+            Toast.makeText(context, "Not currently used", Toast.LENGTH_SHORT).show()
         }
 
         binding.fabAddMemo.setOnClickListener {
