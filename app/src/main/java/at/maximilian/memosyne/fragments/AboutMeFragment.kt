0c78d9a80e866cb40ae0ca77ae2f5398be41c51a
magical-obama <maschwaerzler@a1.net>
2022-10-07 14:34:03 +0200

    Rebase to use view binding

--- app/src/main/java/at/maximilian/memosyne/fragments/AboutMeFragment.kt
+++ app/src/main/java/at/maximilian/memosyne/fragments/AboutMeFragment.kt
@@ -6,13 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import at.maximilian.memosyne.R
+import at.maximilian.memosyne.databinding.FragmentAboutMeBinding
 
 class AboutMeFragment : Fragment() {
+
+    private var _binding: FragmentAboutMeBinding? = null
+
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_about_me, container, false)
+    ): View {
+        _binding = FragmentAboutMeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
     }
 }
